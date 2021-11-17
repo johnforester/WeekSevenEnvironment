3d6ad6b17d3d51aced17cc96f6b5d4045212305e
John Forester <jforeste@Johns-Air.fios-router.home>
2021-11-16 22:56:42 -0500

    Walls

--- WeekSevenEnvironment/WeekSevenEnvironment/Assets/Scripts/FirstPersonMovement.cs
+++ WeekSevenEnvironment/WeekSevenEnvironment/Assets/Scripts/FirstPersonMovement.cs
@@ -9,7 +9,9 @@ public class FirstPersonMovement : MonoBehaviour
     public float speed = 10.0f;
 
     private Rigidbody rb;
+    private bool isGrounded = true;
 
+    [SerializeField] float jumpForce = 3f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +25,29 @@ public class FirstPersonMovement : MonoBehaviour
         rb.MovePosition(rb.position + (localDirection * speed * Time.deltaTime));
     }
 
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
     public void OnPlayerMove(InputValue value)
     {
         Vector2 inputVector = value.Get<Vector2>();
 
         direction.x = inputVector.x;
         direction.z = inputVector.y;
+    }
+
+    public void OnJump()
+    {
+        if (isGrounded)
+        {
+            isGrounded = false;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        
     }
 }
